9de2db7346ae2d547bb27569c3c637fb5706a01c
Dan Corin <dcorin6@gmail.com>
2026-01-21 22:51:18 -0500

    Add cloudflare support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 import vercel from '@astrojs/vercel';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
@@ -58,5 +59,5 @@ export default defineConfig({
       wrap: true,
     },
   },
-  adapter: vercel(),
+  adapter: process.env.CF_PAGES ? cloudflare() : vercel(),
 });
